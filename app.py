f0518d6abf3f127591f0fa69148f660ef835ee70
iyasin <iyasin@umich.edu>
2024-05-02 04:01:26 -0400

    fix

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ supabase: Client = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_
 app = FastAPI()
 
 
+@app.get("/favicon.ico")
+async def get_favicon():
+    raise HTTPException(status_code=404)
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -49,12 +53,18 @@ async def generate_link(parent_id : int, child_id : int):
 
     if parent_id < 0 or child_id < 0:
         return HTTPException(status_code=404, detail="Valid parent and child are required.")
-    
+
     query_string = f"module_id.eq.{parent_id},module_id.eq.{child_id}"
     response = supabase.table('modules').select("*").or_(query_string).execute()
     if len(response.data) != 2:
         return HTTPException(status_code=404, detail=f"Valid parent and child are required. Only {len(response.data)} / 2 items are found in database. Data found: {response.data}") 
-    
+
     data, _ = supabase.table('linkages').insert({'parent': parent_id, 'child': child_id}).execute()
 
     return data
+
+
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
